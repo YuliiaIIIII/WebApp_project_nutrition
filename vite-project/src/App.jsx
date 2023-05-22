0193d29f18c0250d383ecfaa802b95442012177a
YuliiaIIIII <yulyamashka@gmail.com>
2023-05-22 15:55:54 +0300

    added header. changed input field style

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -28,62 +28,22 @@ function App() {
 
 
 
-<header class="bg-orange-200">
-  <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-    <div class="flex lg:flex-1">
-      <a href="#" class="-m-1.5 p-1.5">
-        <span class="sr-only">Your Company</span>
-        <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+<header className="bg-orange-200">
+  <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <div className="flex lg:flex-1">
+      <a href="#" className="-m-1.5 p-1.5">
+        <span className="sr-only">Your Company</span>
+        <img className="h-8 w-auto mr-5" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
         </img>
         </a>
-    </div>
-    <div class="flex lg:hidden">
-      <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
-        <span class="sr-only">Open main menu</span>
-        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-      </button>
-    </div>
-    <div class="hidden lg:flex lg:gap-x-12">
-      <div class="relative">
-        <button type="button" class="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
-          Product
-          <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-          </svg>
-        </button>
-
-      
-        <div class="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-
-          
-        </div>
-      </div>
-
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Features</a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
-      <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
-    </div>
-
-  </nav>
-
-  <div class="lg:hidden" role="dialog" aria-modal="true">
-
-    <div class="fixed inset-0 z-10"></div>
-    
-  </div>
-</header>
-
-
-
-      <header className='mb-9' >
-        <div className="lg:flex lg:items-center lg:justify-between">
-          <div className="min-w-0 flex-1">
+        <div className="min-w-0 flex-1">
             <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Perfect nutrition</h2>
           </div>
+    </div>
 
-          <div className="mt-5 flex lg:ml-4 lg:mt-0">
+    <div className="hidden lg:flex lg:gap-x-12">
+
+      <div className="mt-5 flex lg:ml-4 lg:mt-0">
             <button 
               type="button" 
               className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-indigo-600">
@@ -93,14 +53,26 @@ function App() {
               Favourite
             </button>
           </div>
+    </div>
+
+  </nav>
+
+</header>
+
+
+
+      <header className='mb-9' >
+        <div className="lg:flex lg:items-center lg:justify-between">
+
         </div>
       </header>
 
       <div>
-        <div className="flex flex-row justify-around text-base mb-9">
+        <div className="flex flex-row justify-center text-base mb-9">
           <input 
             type="text" 
-            className='border-2 rounded border-solid border-black px-2 py-1.5'
+            className='border border-1 rounded-l-md border-solid border-blue-800 px-2 py-1.5'
+            placeholder="What are you looking for?"
             value={inputText} 
             onChange={(event) => setInputText(event.target.value)}
           />
@@ -108,13 +80,13 @@ function App() {
           <button 
             type="button" 
             onClick={() => setServerRequestText(inputText)} 
-            className='inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-indigo-600'
+            className='inline-flex items-center rounded-r-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-indigo-600'
           >
             Find recipe
           </button>
         </div>
 
-        <div className='w-1/2 flex flex-center ml-auto mr-auto mt-9 mb-8'>
+        <div className='w-1/2 flex justify-center ml-auto mr-auto mt-9 mb-8'>
           <input
             type="radio" 
             value="ingridients" 
--- vite-project/src/Modal.jsx
+++ vite-project/src/Modal.jsx
@@ -21,6 +21,7 @@ function Modal({changeActive}){
                   <div className="mt-2">
                     <img src={data?.image} alt="img" />
                     <p className="text-sm text-gray-500">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem, nesciunt aperiam suscipit molestiae similique, distinctio maiores eos blanditiis et dolor molestias ipsa praesentium laboriosam eligendi reprehenderit accusamus? Ratione, nostrum temporibus.
                       
                       {data?.extendedIngredients.original}
                     </p>
@@ -29,7 +30,7 @@ function Modal({changeActive}){
               </div>
             </div>
             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              <button type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 sm:ml-3 sm:w-auto">Button</button>
+              <button type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 sm:ml-3 sm:w-auto">Add to fav</button>
               <button 
                 type="button" 
                 className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
