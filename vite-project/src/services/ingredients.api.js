eac1b10ed1e9db38f12b39942e6af3cfbabf9f21
YuliiaIIIII <yulyamashka@gmail.com>
2023-05-29 19:07:09 +0200

    clear some comments

--- vite-project/src/services/ingredients.api.js
+++ vite-project/src/services/ingredients.api.js
@@ -38,6 +38,4 @@ export const ingredientsApi = createApi({
    }),
 })
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
 export const { useGetByIngridientsQuery, useGetByNutrientsQuery, useGetRecipesInformationQuery } = ingredientsApi;
